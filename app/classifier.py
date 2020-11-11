92aeda05e7af4ca144bb9e6ebcb4ee7842da19e3
Florian ERNST <florianernst59@gmail.com>
2020-11-11 18:56:15 +0100

    Added model & dataset

---
+++ app/classifier.py
@@ -0,0 +1,51 @@
+from typing import Literal
+
+from sklearn.metrics import accuracy_score
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+classifier = SentimentIntensityAnalyzer()
+
+Sentiment = Literal['positive', 'neutral', 'negative']
+
+
+def get_sentiment_from_compound(compound_score: float) -> Sentiment:
+    """ Return the sentiment from a compound score. """
+    if compound_score >= 0.05:
+        return 'positive'
+    if compound_score <= -0.05:
+        return 'negative'
+
+    return 'neutral'
+
+
+def predict(text: str) -> Sentiment:
+    """ Predict the sentiment of a given text. """
+    predictions = classifier.polarity_scores(text)
+    return get_sentiment_from_compound(predictions['compound'])
+
+
+def benchmark_model():
+    """ Test the Vader model on the Tweets dataset. """
+    scores = []
+    predicted_scores = []
+
+    with open('tweets_GroundTruth.txt') as f:
+        for line in f.readlines():
+            _, score, text = line.strip().split('\t')
+
+            score = float(score)
+            predicted_score = predict(text)
+
+            scores.append(get_sentiment_from_compound(score))
+            predicted_scores.append(predicted_score)
+
+    print(f'Accuracy score: {accuracy_score(scores, predicted_scores):.2%}')
+
+
+def main():
+    # Run the demo
+    benchmark_model()
+
+
+if __name__ == '__main__':
+    main()
